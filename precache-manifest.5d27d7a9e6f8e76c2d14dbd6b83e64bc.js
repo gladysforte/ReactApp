a083ba1e4fb52416e3a12df84885b0570301331a
gladys <gladys@tirsolutions.com>
2020-08-09 21:17:06 +0800

    Updates

---
+++ precache-manifest.5d27d7a9e6f8e76c2d14dbd6b83e64bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12dc635139906f47694000c0f5a184e1",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "711017e2393d943c5245",
+    "url": "/ReactApp/static/css/main.7fd62092.chunk.css"
+  },
+  {
+    "revision": "e22a9b25e13268c336e1",
+    "url": "/ReactApp/static/js/2.490c675e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactApp/static/js/2.490c675e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711017e2393d943c5245",
+    "url": "/ReactApp/static/js/main.d2760349.chunk.js"
+  },
+  {
+    "revision": "f7a849198eb185196ff8",
+    "url": "/ReactApp/static/js/runtime-main.684e7a07.js"
+  }
+]);
